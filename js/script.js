69ec61f3d505e761a2ac11fdf2e8d835fbb1e283
AeksandraKorovko <alextim77733@gmail.ru>
2019-08-10 13:29:24 +0300

    test

---
+++ js/script.js
@@ -0,0 +1,19 @@
+jQuery(document).ready(function () {
+
+
+$(function(){
+$('.bloxPopup').lightboxController({
+        appendRegion: '.container',
+        lightboxID:   'lightbox',
+        fadeDuration:   300,
+        contentFade:  300,
+        closeBtn:   true,
+    });
+});
+}); 
+
+
+
+
+
+
